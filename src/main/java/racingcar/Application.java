a84a94fab00e561adbc183e99752e33353979e57
윤도균 <ehrbs4252@gmail.com>
2025-10-27 13:26:37 +0900

    refator: Application 간소화

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,25 +1,10 @@
 package racingcar;
 
-import racingcar.domain.RacingGame;
-import racingcar.view.InputView;
-import racingcar.view.OutputView;
+import racingcar.controller.RacingController;
 
 public class Application {
     public static void main(String[] args) {
-        InputView inputView = new InputView();
-        OutputView outputView = new OutputView();
-        
-        String carNames = inputView.readCarNames();
-        int attemptCount = inputView.readAttemptCount();
-        
-        RacingGame game = new RacingGame(carNames);
-        outputView.printResultMessage();
-        
-        for (int i = 0; i < attemptCount; i++) {
-            game.playRound();
-            outputView.printRoundResult(game.getCars());
-        }
-        
-        outputView.printWinners(game.getWinners());
+        RacingController controller = new RacingController();
+        controller.start();
     }
 }
