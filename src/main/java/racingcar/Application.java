43b8486707fac23e1462dbf2d0593f5ab214bdb0
윤도균 <ehrbs4252@gmail.com>
2025-10-27 12:16:35 +0900

    feat: InputView 동작 확인

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,20 +1,25 @@
 package racingcar;
 
 import racingcar.domain.RacingGame;
+import racingcar.view.InputView;
 import racingcar.view.OutputView;
 
 public class Application {
     public static void main(String[] args) {
+        InputView inputView = new InputView();
         OutputView outputView = new OutputView();
-        RacingGame game = new RacingGame("pobi,woni,jun");
-
+        
+        String carNames = inputView.readCarNames();
+        int attemptCount = inputView.readAttemptCount();
+        
+        RacingGame game = new RacingGame(carNames);
         outputView.printResultMessage();
-
-        for (int i = 0; i < 5; i++) {
+        
+        for (int i = 0; i < attemptCount; i++) {
             game.playRound();
             outputView.printRoundResult(game.getCars());
         }
-
+        
         outputView.printWinners(game.getWinners());
     }
 }
